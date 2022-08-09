2019d20da6dd5e1521f205386f51a76f76160da2
Martin Fuenzalida <mafuenzalida@uc.cl>
2022-08-09 13:37:50 -0400

    chore: improve getExpensesByUser query

--- packages/domains/expense/data/db-expense.ts
+++ packages/domains/expense/data/db-expense.ts
@@ -2,23 +2,22 @@ import knex from '@nc/utils/db/knex';
 import { QueryOptions } from '@nc/utils/types';
 
 async function getExpensesByUser(userId: string, options?: QueryOptions) {
+  const query = knex.select('*')
+    .from('expenses')
+    .where('user_id', userId)
+  
   if (options.orderBy) {
-    return await knex.select('*')
-    .from('expenses')
-    .where('user_id', userId)
-    .orderBy(options.sortBy,options.orderBy)
-    .paginate({ 
+    query.orderBy(options.sortBy,options.orderBy)
+  }
+  
+  try {
+    return await query.paginate({ 
       perPage: options.perPage || 10,
       currentPage: options.currentPage || 1,
     })
-  } else {
-    return await knex.select('*')
-    .from('expenses')
-    .where('user_id', userId)
-    .paginate({ 
-      perPage: options.perPage || 10,
-      currentPage: options.currentPage || 1,
-    })
+  }
+  catch(error) {
+    throw error;
   }
 }
 
